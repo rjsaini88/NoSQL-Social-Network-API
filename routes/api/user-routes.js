f6d021eb520fecba1a24764bf56ca62eb456d1e1
Rj Saini <rsaini88@gmail.com>
2023-02-19 21:17:47 -0800

    completed user routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,40 +1,100 @@
-
-const router = require('express').Router();
-const {User} = require("../../models")
+const router = require("express").Router();
+const { User } = require("../../models");
 
 //TODO - ROUTE THAT GETS ALL THE USERS, include friends?
-router.get('/', (req,res)=> {
-
-})
+router.get("/", (req, res) => {
+  // User.find({}, (err, user)=> {
+  //     res.status(200).json(user);
+  // })
+  User.find()
+    .then((user) => res.json(user))
+    .catch((err) => res.status(500).json(err));
+});
 
 //TODO - ROUTE THAT CREATES A NEW USER
-router.post('/', (req,res)=> {
+router.post("/", (req, res) => {
+  User.create(req.body)
+    .then((newUser) =>
+      !user
+        ? res.status(404).json({ message: "User was not created!" })
+        : res.json(newUser)
+    )
+    .catch((err) => res.status(500).json(err));
 
+  // .then((users) => res.json(users))
+  // .catch((err) => {
+  //   console.log(err);
+  //   return res.status(500).json(err);
+  // });
 });
 
 //TODO - ROUTE THAT GETS A SINGLE USER BASED ON USER ID
-router.get('/:userId', (req,res) => {
-
-})
+router.get("/:userId", (req, res) => {
+  User.findOne({ _id: req.params.userId })
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "No user found with that id!" })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+});
 
 //TODO - ROUTE THAT UPDATES A SINGLE USER
-router.put('/:userId', (req,res)=> {
-
-})
+router.put("/:userId", (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.params.userId },
+    { $set: req.body },
+    { runValidators: true, new: true }
+  )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "No user found with this id!" })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+});
 
 //TODO - ROUTE THAT DELETES A SINGLE USER BASED ON USER ID -- check assignment 6
-router.delete('/:userId', (req,res)=> {
-
+router.delete("/:userId", (req, res) => {
+  User.findOneAndDelete({ _id: req.params.userId }, (err, user) => {
+    if (user) {
+      res.status(200).json(user);
+      console.log(`Deleted: ${user}`);
+    } else {
+      console.log("Something went wrong");
+      res.status(500).json({ message: "Something went wrong" });
+    }
+  });
 });
 
 //TODO - ROUTE THAT ADDS A FRIEND TO A USER
-router.put('/:userId/friends/:friendId', (req,res)=> {
-
-})
+router.put("/:userId/friends/:friendId", (req, res) => {
+  User.findByIdAndUpdate(
+    { _id: req.params.userId },
+    { $addToSet: { friends: req.body } },
+    { runValidators: true, new: true }
+  )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "No user found with this id!" })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
+});
 
 //TODO - ROUTE THAT DELETES A FRIEND FROM A USER'S FRIENDS, DONT DELETE THE FRIEND AS A USER THOUGH!
-router.delete('/:userId/friends/:friendId', (req,res)=> {
-  
+router.delete("/:userId/friends/:friendId", (req, res) => {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    { $pull: { friends: { friendId: req.params.friendId } } },
+    { runValidators: true, new: true }
+  )
+    .then((user) =>
+      !user
+        ? res.status(404).json({ message: "No user found with that ID :(" })
+        : res.json(user)
+    )
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
